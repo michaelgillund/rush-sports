431963999100af7183c598da3c3b3ef65cfb41c5
Michael Gillund <michaelgillund@gmail.com>
2022-10-04 23:03:23 -0500

    Initial Commit

---
+++ Rush/RushApp.swift
@@ -0,0 +1,17 @@
+//
+//  RushApp.swift
+//  Rush
+//
+//  Created by Michael Gillund on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
